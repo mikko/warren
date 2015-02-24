5e06cb1d907e50c76bee79868174f53339c60560
Mikko Pohja <mikko.pohja@futurice.com>
2015-02-24 23:35:03 +0200

    Updated pages

---
+++ main.js
@@ -0,0 +1,23 @@
+require([
+    "../lib/warren"
+],function(
+	WarrenGraph
+){
+    var data = [
+        ["Leporidae", "Lepus"],
+        ["Lagomorpha", "Leporidae"],
+        ["Eutheria", "Lagomorpha"],
+        ["Leporidae", "Sylvilagus"],
+        ["Sylvilagus", "Sylvilagus insonus"],
+        ["Sylvilagus", "Sylvilagus bachmani"],
+        ["Sylvilagus", "Sylvilagus palutris"],
+        ["Leporidae", "Oryctolagus"]
+    ];
+
+    var graphOptions = {
+        parentEl: document.getElementById("graph"),
+        data: data
+    };
+    var g = new WarrenGraph(graphOptions);
+    document.getElementById('data').innerText = "Data used:\n" + JSON.stringify(data, null, 2);
+});
